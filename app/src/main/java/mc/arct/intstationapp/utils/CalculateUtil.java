c2c3b4045ff37abfd63e6d819c217e7cb32de982
Androbo-MC <n.sekiguchi@androbo.jp>
2017-12-16 16:58:40 +0900

    [川﨑]計算(共通)用クラスの作成

---
+++ app/src/main/java/mc/arct/intstationapp/utils/CalculateUtil.java
@@ -0,0 +1,27 @@
+package mc.arct.intstationapp.utils;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import mc.arct.intstationapp.models.StationDetailVO;
+import mc.arct.intstationapp.models.StationDistanceVO;
+import mc.arct.intstationapp.storage.StationDAO;
+
+/**
+ * 計算共通クラス
+ */
+
+public class CalculateUtil {
+
+    /**
+     * 中間地点を計算するメソッド
+     *
+     * @param latList 計算に使う全駅の緯度リスト
+     * @param lngList 計算に使う全駅の経度リスト
+     * @return LatLng 中間地点の座標オブジェクト
+     */
+
+}
