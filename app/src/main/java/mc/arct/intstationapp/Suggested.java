48881f107a51a6c14d3d68100315c8e508159a1c
Androbo-MC <n.sekiguchi@androbo.jp>
2017-12-17 21:54:32 +0900

    [川﨑]候補駅画面用クラスの作成(画面遷移以外)

--- app/src/main/java/mc/arct/intstationapp/Suggested.java
+++ app/src/main/java/mc/arct/intstationapp/Suggested.java
@@ -1,7 +1,16 @@
 package mc.arct.intstationapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+import mc.arct.intstationapp.models.StationDetailVO;
+import mc.arct.intstationapp.models.StationDistanceVO;
+import mc.arct.intstationapp.utils.CalculateUtil;
 
 /**
  * 候補駅
@@ -9,9 +18,31 @@ import android.support.v7.app.AppCompatActivity;
 
 public class Suggested extends AppCompatActivity{
 
+    private ArrayList<StationDetailVO> stationList = new ArrayList<>();
+    private ArrayList<StationDistanceVO> stationDistanceList = new ArrayList<>();
+    public static int FVP = 0;
+    public static int y = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.s011_suggested);
+
+        // アプリケーションクラスのインスタンスを取得(今回は無し)
+
+        // 全画面から駅情報リストを受け取る
+        Intent intent = getIntent();
+        stationList = (ArrayList<StationDetailVO>) intent.getSerializableExtra("stationList");
+        double centerLat = intent.getDoubleExtra("centerLat", 0);
+        double centerLng = intent.getDoubleExtra("centerLng", 0);
+        LatLng centerLatLng = new LatLng(centerLat, centerLng);
+
+        // ここではソートされた駅情報をリストごと取得
+        stationDistanceList
+                = CalculateUtil.calcNearStationsList(centerLatLng, getApplicationContext());
+        // 1000件以上もいらないので上位10件以外削除
+        stationDistanceList.subList(10, stationDistanceList.size()).clear();
     }
+
+
 }
