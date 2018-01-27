732aa7f0e91768df62b63a176cd358190caed7ae
Androbo-MC <n.sekiguchi@androbo.jp>
2018-01-27 18:59:06 +0900

    [川﨑]検索結果の文字表示を修正しました。

--- app/src/main/java/mc/arct/intstationapp/activities/Result.java
+++ app/src/main/java/mc/arct/intstationapp/activities/Result.java
@@ -2,8 +2,9 @@ package mc.arct.intstationapp.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
-import android.support.v7.app.AlertDialog;
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.MotionEvent;
@@ -14,10 +15,14 @@ import android.widget.TextView;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
+import java.util.Timer;
 
 import mc.arct.intstationapp.R;
 import mc.arct.intstationapp.models.StationDetailVO;
 import mc.arct.intstationapp.models.StationDistanceVO;
+import mc.arct.intstationapp.models.StationTransferVO;
+import mc.arct.intstationapp.network.JorudanInfoTask;
+import mc.arct.intstationapp.network.MyNetworkManager;
 import mc.arct.intstationapp.storage.StationDAO;
 import mc.arct.intstationapp.utils.CalculateUtil;
 import mc.arct.intstationapp.utils.IntentUtil;
@@ -32,6 +37,9 @@ public class Result extends AppCompatActivity{
     private StationDetailVO resultStation;
     private LatLng centerLatLng;
     private TextView searchResult;
+    // タスクを実行するタイマー
+    private Timer timer;
+    Handler handler;
     // 全アクティビティで使えるアプリケーションクラス（今回は無し）
     // 文字スクロール制御フラグ
     private boolean is_scroll = false;
@@ -49,7 +57,7 @@ public class Result extends AppCompatActivity{
         layout = findViewById(R.id.result_train);
         detector = new ScaleGestureDetector(this, new ScaleListener());
 
-        // 入力画面から入力駅情報リストを受け取る
+        // 遷移前画面から入力駅情報リストを受け取る
         Intent intent = getIntent();
         stationList =
                 (ArrayList<StationDetailVO>) intent.getSerializableExtra("result");
@@ -95,7 +103,14 @@ public class Result extends AppCompatActivity{
         );
     }
 
-    // todo:共有ボタンが押された時
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        // アクティビティが落ちる時にタイマーも終了させる
+        timer.cancel();
+    }
+
+    // 共有ボタンが押された時
     public void callLINE(View v) {
 
         // LINE共有機能を呼び出す
@@ -103,21 +118,64 @@ public class Result extends AppCompatActivity{
         // Intentが返却されていたら、LINE連携へ遷移する
         if (obj instanceof Intent) {
         // AlertDialog.Builderが返却されていたら、遷移せずダイアログを表示
-        } else if (obj instanceof AlertDialog.Builder);
+        } else if (obj instanceof AlertDialog.Builder) {
+            AlertDialog.Builder dialog = (AlertDialog.Builder)obj;
+            dialog.show();
+        }
 
     }
 
     // 周辺情報ボダンが押された時
     public void callMapInfo(View v) {
-        // 画面遷移処理で、入力されていた駅情報のリストと候補駅を次の画面に送る
-        Intent intent = IntentUtil.prepareForArea(Result.this,
-                stationList, resultStation);
+        // ネットワークの接続状態を確認
+        AlertDialog.Builder dialog = MyNetworkManager.checkConnection(this);
+        if (dialog == null) {
+            // 画面遷移処理で、入力されていた駅情報のリストと候補駅を次の画面に送る
+            Intent intent = IntentUtil.prepareForArea(Result.this,
+                    stationList, resultStation);
+            startActivity(intent);
+        } else {
+            // 電波がなかったらダイアログを出す
+            dialog.show();
+        }
+    }
+
+    // 候補駅ボタンが押された時
+    public void callSuggestedStations(View v) {
+        // 画面遷移処理で、入力されていた駅情報のリストと中間地点座標を次の画面に送る
+        Intent intent = IntentUtil.prepareForSuggested(Result.this,
+                stationList, centerLatLng.latitude, centerLatLng.longitude);
         startActivity(intent);
     }
 
-    // todo:候補駅ボタンが押された時
-
-    // todo:ルートボタンが押された時
+    // ルートボタンが押された時
+    public void callRoute(View v) {
+        //　ネットワークの接続状態を確認
+        AlertDialog.Builder dialog = MyNetworkManager.checkConnection(this);
+        if (dialog == null) {
+            // ジョルダンに経路検索のリクエストを送る
+            final JorudanInfoTask jit = new JorudanInfoTask(this, stationList, resultStation.getJorudanName());
+            // ここから非同期処理終了後の処理を記述する
+            jit.setOnCallBack(new JorudanInfoTask.CallBackTask(){
+                @Override
+                public void CallBack() {
+                    super.CallBack();
+                    // 結果の取得
+                    ArrayList<StationTransferVO>[] resultInfoLists = jit.getResultInfoLists();
+                    // 画面遷移処理で、入力されていた駅情報のリストと候補駅を次の画面に送る
+                    Intent intent = IntentUtil.prepareForRoute(Result.this,
+                            stationList, resultStation, centerLatLng.latitude, centerLatLng.longitude,
+                            resultInfoLists);
+                    startActivity(intent);
+                }
+            });
+            // 非同期処理の実行
+            jit.execute();
+        } else {
+            // 電波なかったらダイアログ出す
+            dialog.show();
+        }
+    }
 
     // ピンチアウト用に作成
     public boolean onTouchEvent(MotionEvent event) {
