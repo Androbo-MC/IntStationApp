0faa8468170ffd71db980e2d665831b166e16648
Androbo-MC <n.sekiguchi@androbo.jp>
2018-02-15 22:23:22 +0900

    [川﨑]文字表示の修正(未完了)

--- app/src/main/java/mc/arct/intstationapp/activities/Result.java
+++ app/src/main/java/mc/arct/intstationapp/activities/Result.java
@@ -1,6 +1,7 @@
 package mc.arct.intstationapp.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
@@ -16,6 +17,7 @@ import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
 import java.util.Timer;
+import java.util.TimerTask;
 
 import mc.arct.intstationapp.R;
 import mc.arct.intstationapp.models.StationDetailVO;
@@ -37,9 +39,12 @@ public class Result extends AppCompatActivity{
     private StationDetailVO resultStation;
     private LatLng centerLatLng;
     private TextView searchResult;
+    private TextView searchResultKana;
     // タスクを実行するタイマー
     private Timer timer;
     Handler handler;
+    // 点滅状態を保持する
+    private boolean isBlink = false;
     // 全アクティビティで使えるアプリケーションクラス（今回は無し）
     // 文字スクロール制御フラグ
     private boolean is_scroll = false;
@@ -101,6 +106,30 @@ public class Result extends AppCompatActivity{
                     }
                 }
         );
+        // 駅名を点滅させるタイマーを設定する
+        timer = new Timer();
+        handler = new Handler();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                // 処理をUIスレッドに送る
+                handler.post(new Runnable() {
+                    public void run() {
+                        // 点滅フラグがfalseなら点灯させる
+                        if (!isBlink) {
+                            searchResult.setTextColor(Color.MAGENTA);
+                            searchResultKana.setTextColor(Color.MAGENTA);
+                            isBlink = true;
+                        } else {
+                            searchResult.setTextColor(Color.BLACK);
+                            searchResultKana.setTextColor(Color.BLACK);
+                            isBlink = false;
+                        }
+                    }
+                });
+            }
+        // 1000ミリ秒後に1000ミリ秒間隔でタスク実行
+        }, 1000, 1000);
     }
 
     @Override
