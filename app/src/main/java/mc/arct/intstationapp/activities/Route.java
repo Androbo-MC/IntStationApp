95981d682afd389abd59b37fcc5b2fc1ebf27093
Androbo-MC <n.sekiguchi@androbo.jp>
2018-02-18 00:04:34 +0900

    [川﨑]ルート画面用のものを複数追加しました(準備のみ)

--- app/src/main/java/mc/arct/intstationapp/activities/Route.java
+++ app/src/main/java/mc/arct/intstationapp/activities/Route.java
@@ -1,15 +1,18 @@
 package mc.arct.intstationapp.activities;
 
 import android.os.Bundle;
+
 import android.support.v7.app.AppCompatActivity;
 
+
 import mc.arct.intstationapp.R;
+
 
 /**
  * ルート
  */
 
-public class Route extends AppCompatActivity {
+public class Route extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/mc/arct/intstationapp/fragments/PageFragment.java
@@ -0,0 +1,11 @@
+package mc.arct.intstationapp.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * ルート画面のタブ管理用フラグメント
+ */
+
+public class PageFragment extends Fragment {
+
+}
